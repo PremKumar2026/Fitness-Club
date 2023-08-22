6324b1c91235a229f03ca32399606ae0c3b0d2da
PremKumar2026 <125431380+PremKumar2026@users.noreply.github.com>
2023-08-22 00:20:05 -0400

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+
+const Footer = () => (
+  <Box mt="80px" bgcolor="#FFF3F4">
+    <Typography variant="h5" sx={{ fontSize: { lg: '28px', xs: '20px' } }} mt="41px" textAlign="center" pt="20px" pb="20px">Sculpting Futures, One Rep at a Time. </Typography>
+  </Box>
+);
+
+export default Footer;
---
+++ src/utils/fetchData.js
@@ -0,0 +1,22 @@
+export const exerciseOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+  },
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': '4b0c6e5911mshadf5444dbe7c9e5p1d5938jsnb6e77c88cf7e',
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
